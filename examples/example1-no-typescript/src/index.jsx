fe85f1b90827678fc5327cb32f55f0ba31084bb2
Benedicte Giraud <benegir@gmail.com>
2025-01-08 15:50:50 +0100

    chore: update examples (#80)

--- examples/example1-no-typescript/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './example';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-module.hot.accept();
