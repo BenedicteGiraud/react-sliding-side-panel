0c0e0f455c5a3917359ad330b1796b78f6e368cc
Benedicte Giraud <benegir@gmail.com>
2025-01-25 14:26:53 +0100

    remove stories folder

--- src/__docs__/SlidingPanel.stories.tsx
+++ src/__docs__/SlidingPanel.stories.tsx
@@ -1,8 +1,8 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import Example from "./Example";
+import type { Meta, StoryObj } from '@storybook/react';
+import Example from './Example';
 
 const meta: Meta<typeof Example> = {
-  title: "SlidingPanel",
+  title: 'SlidingPanel',
   component: Example,
 };
 
@@ -11,7 +11,7 @@ type Story = StoryObj<typeof Example>;
 
 export const Default: Story = {
   args: {
-    type: "left",
+    type: 'left',
     size: 30,
     noBackdrop: false,
   },
