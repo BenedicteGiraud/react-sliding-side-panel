f982b74b92a3d21f654d81d3fa955d590c349ff6
Benedicte Giraud <benegir@gmail.com>
2025-03-21 23:13:55 +0100

    remove unused examples (#95)

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,10 +5,7 @@ welcome, from issue reports to PRs and documentation / write-ups.
 
 Before you open a PR:
 
-- In development, go in `examples/example1` and run `npm start` to build (and watch) the project source, and run
-  the [development server](http://localhost:8080).
-- Please ensure all the examples work correctly after your change. If you're
-  adding a major new use-case, add a new example `examples/`.
+- In development, go in `examples/typescript` and run `npm run dev` after installing the dependencies.
 - Ensure that your effort is aligned with the project's roadmap by talking to
   the maintainers, especially if you are going to spend a lot of time on it.
 - Make sure there's an issue open for any work you take on and intend to submit
--- examples/typescript/src/App.tsx
+++ examples/typescript/src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import SlidingPanel from 'react-sliding-side-panel';
+import { useState } from 'react';
+import SlidingPanel, { PanelType } from '../../../src';
 import './App.css';
-import 'react-sliding-side-panel/lib/index.css';
+import '../../../src/index.css';
 
 const App = () => {
-  const [openPanel, setOpenPanel] = useState(false);
-  const [panelType, setPanelType] = useState('left');
-  const [panelSize, setPanelSize] = useState(30);
-  const [noBackdrop, setNoBackdrop] = useState(false);
+  const [openPanel, setOpenPanel] = useState<boolean>(false);
+  const [panelType, setPanelType] = useState<PanelType>('left');
+  const [panelSize, setPanelSize] = useState<number>(30);
+  const [noBackdrop, setNoBackdrop] = useState<boolean>(false);
 
   return (
     <div className="example-container">
@@ -82,6 +82,7 @@ const App = () => {
         backdropClicked={() => setOpenPanel(false)}
         size={panelSize}
         panelClassName="additional-class"
+        panelContainerClassName=""
         noBackdrop={noBackdrop}
       >
         <div className="panel-container">
